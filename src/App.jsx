7c6317b0c9704cfa75a2a5df4baee6af3692478e
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-11-23 16:39:46 +0530

    feat: enable routing for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages";
+
 function App() {
   return <div className="App">
-    Hello World
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   </div>;
 }
 
