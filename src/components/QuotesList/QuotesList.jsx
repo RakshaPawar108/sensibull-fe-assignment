d1accab37a706d120cc129be1dc60b321f008700
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-11-26 12:24:36 +0530

    feat: refresh quotes after valid till time expires

--- src/components/QuotesList/QuotesList.jsx
+++ src/components/QuotesList/QuotesList.jsx
@@ -6,14 +6,35 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import { SortOptions } from "../SortOptions/SortOptions";
+import moment from "moment/moment";
 
 export const QuotesList = () => {
   const location = useLocation().pathname.split("/")[2];
   const [quotes, setQuotes] = useState([]);
+  const [validTill, setValidTill] = useState();
+  const [currentTime, setCurrentTime] = useState();
 
   useEffect(() => {
     getQuotes(setQuotes, location);
   }, [location]);
+
+  useEffect(() => {
+    const currentTime_array = quotes.map((item) => item.time);
+    currentTime_array.sort((a, b) => (a > b ? 1 : b > a ? -1 : 0));
+    setCurrentTime(currentTime_array[0]);
+    const valid_till_array = quotes.map((item) => item.valid_till);
+    valid_till_array.sort((a, b) => (a > b ? 1 : b > a ? -1 : 0));
+    setValidTill(valid_till_array[0]);
+    console.log(validTill, currentTime);
+
+    const interval = setInterval(
+      () => getQuotes(setQuotes, location),
+      new Date(validTill) - new Date(currentTime)
+    );
+    return () => {
+      clearInterval(interval);
+    };
+  }, [validTill, currentTime, quotes, location]);
 
   const sortByTime = (sortVal) => {
     let sortedQuotes = quotes;
@@ -50,10 +71,18 @@ export const QuotesList = () => {
                   <ListItemText primary={`Price: ${quote.price}`} />
                 </ListItemButton>
                 <ListItemButton>
-                  <ListItemText primary={`Time: ${quote.time}`} />
+                  <ListItemText
+                    primary={`Time: ${moment(quote.time).format(
+                      "MMMM Do YYYY, h:mm:ss a"
+                    )}`}
+                  />
                 </ListItemButton>
                 <ListItemButton>
-                  <ListItemText primary={`Valid Till: ${quote.valid_till}`} />
+                  <ListItemText
+                    primary={`Valid Till: ${moment(quote.valid_till).format(
+                      "MMMM Do YYYY, h:mm:ss a"
+                    )}`}
+                  />
                 </ListItemButton>
               </ListItem>
             );
