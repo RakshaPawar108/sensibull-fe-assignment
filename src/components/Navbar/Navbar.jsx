cf85aa34767b671b1f9444c22aeca4de547d465b
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-11-27 09:35:53 +0530

    style: add styling to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,31 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import { bullImg } from "../../assets";
+import "./Navbar.css";
+import { Typography } from "@mui/material";
 
 export function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        className="navbar"
+        sx={{ backgroundColor: "white" }}
+      >
         <Toolbar>
+          <div className="image-wrapper">
+            <img className="navbar-icon" src={bullImg} alt="bull icon" />
+          </div>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            className="brand-name"
+          >
+            Sensibull
+          </Typography>
         </Toolbar>
       </AppBar>
     </Box>
